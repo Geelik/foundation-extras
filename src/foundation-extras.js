6926e093f1e7a755d24b6412b395523252864cdf
web_thibaultp <t.picard@webetsolutions.com>
2018-12-27 15:49:59 +0100

    Fix event not triggering

--- src/foundation-extras.js
+++ src/foundation-extras.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	const aValidSizes = ['small', 'medium', 'large'];
 	$(window).on('changed.zf.mediaquery', function(oEvent, sNewSize, sOldSize) {
-		if (aValidSizes.includes(sNewSize) && aValidSizes.includes(sOldSize))
+		if (aValidSizes.includes(sNewSize) || aValidSizes.includes(sOldSize))
 		{
 			$(window).trigger('mq:change:'+sNewSize, [sOldSize]);
 		}
